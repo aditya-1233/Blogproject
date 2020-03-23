746b93ef0f1456db0ba6dca174208a1ad18515e2
Aditya Behera <adityababa1233@gmail.com>
2020-03-23 23:53:27 +0530

    Demoapp added

--- advanceproj/settings.py
+++ advanceproj/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'demoapp.apps.DemoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'demoapp'
 ]
 
 MIDDLEWARE = [
